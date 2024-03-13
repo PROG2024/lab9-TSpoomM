22f532a3a8d6d594246396d48962370ad0d1e81a
Tanapoom Srikaewkheaw <tanapoomts1350@gmail.com>
2024-03-13 12:30:43 +0700

    All Done.

--- counter/counter.py
+++ counter/counter.py
@@ -9,14 +9,47 @@
       You may not have a __new__ depending on how you implement the singleton.
 """
 
+
 class Counter:
+    """
+    Examples :
+        >>> counter = Counter()
+        >>> counter.count
+        0
+        >>> counter.count        # invoking count doesn't change anything
+        0
+        >>> counter.increment()  # add 1 and return the new count
+        1
 
-    def __init__(self):
-        self.__count = 0
+        >>> counter2 = Counter()
+        >>> counter2 is counter
+        True
+        >>> counter2.count       # shares same count
+        1
+        >>> counter2.increment()  # add 1 and return the new count
+        2
+        >>> counter.count
+        2
+    """
 
+    _instance = None
+
+    # def __init__(self):
+    #     self.__count = 0
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.__count = 0
+        return cls._instance
 
     def __str__(self):
         return f"{self.__count}"
 
-    #TODO write count property
-    #TODO write increment method
+    @property
+    def count(self):
+        return self.__count
+
+    def increment(self):
+        self.__count += 1
+        return self.__count
--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,46 @@
 
    You can use pytest or unittest.
 """
+
+import unittest
+from counter import Counter
+
+
+class TestCounterSingleton(unittest.TestCase):
+    def test_singleton_instance(self):
+        # Create two instances of Counter
+        counter1 = Counter()
+        counter2 = Counter()
+
+        # Verify that both instances are the same
+        self.assertNotEqual(counter1, Counter)
+        self.assertNotEqual(counter2, Counter)
+        self.assertIs(counter1, counter2)
+
+    def test_shared_count(self):
+        # Create two instances of Counter
+        counter1 = Counter()
+        counter2 = Counter()
+
+        # Increment count using one instance
+        counter1.increment()
+
+        # Verify that count is shared between instances
+        self.assertEqual(counter1.count, counter2.count)
+
+        # Increment count using one instance
+        counter2.increment()
+
+        # Verify that count is shared between instances
+        self.assertEqual(counter1.count, counter2.count)
+
+    def test_count_not_reset(self):
+        # Create an instance of Counter and increment count
+        counter1 = Counter()
+        counter1.increment()
+
+        # Create another instance of Counter
+        counter2 = Counter()
+
+        # Verify that count is not reset to 0
+        self.assertNotEqual(counter2.count, 0)
